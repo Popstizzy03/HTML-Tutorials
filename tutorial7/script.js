009deb270686499612ec8389343b89fe1317a9e7
Rabboni Kabongo <kabongorabboni03@gmail.com>
2025-05-26 14:02:49 +0200

    tailwind initialization

---
+++ tutorial7/script.js
@@ -0,0 +1,12 @@
+        tailwind.config = {
+            darkMode: 'class',
+            theme: {
+                extend: {
+                    colors: {
+                        primary: '#5D5CDE',
+                        'primary-dark': '#4A49B0',
+                        'primary-light': '#7A79E6',
+                    }
+                }
+            }
+        }
